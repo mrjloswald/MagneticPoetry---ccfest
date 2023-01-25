157093a83bf1fd10c370cb6d90f54cd3c0e66c69
Mr. Oswald <62300716+gajoswald@users.noreply.github.com>
2023-01-25 15:45:14 -0500

    Minor adjustments
    Change rectMode and use padding everywhere

--- progression/script.js
+++ progression/script.js
@@ -8,26 +8,28 @@ function setup() {
   createCanvas(400, 400)
   words = shuffle(words)
   noLoop()
+  rectMode(CENTER)
 }
 
 function draw() {
   background('white')
   const padding = 2
   let x = padding
-  let dy = 2 + textSize()
+  let dy = padding + textSize()
   let y = dy
 
   for( const word of words ) {
     const tw  = textWidth(word)
+    const blockWidth = tw + padding + padding
     if( x + tw > width ) {
       y += dy
       x = padding
     }
     push()
-    translate(x,y)
-    rect( 0, 0 - textSize(), tw + 4, textSize() )
-    text(word,0 + 2,0)
+    translate(x + blockWidth/2,y, textSize()/2)
+    rect( 0, 0, blockWidth, textSize() )
+    text(word,0 - tw/2, 0 + textSize()/2)
     pop()
-    x += textWidth(word) + padding + 4
+    x += tw + padding + padding + padding
   }
 }
