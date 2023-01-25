d8943a931b151365a3e94173b9faafc8667d8b45
Mr. Oswald <62300716+gajoswald@users.noreply.github.com>
2023-01-25 15:47:30 -0500

    Add hovering
    created isMouseOver and background changing capabilities

--- progression/script.js
+++ progression/script.js
@@ -1,5 +1,5 @@
 let words
-
+let containers = [] 
 function preload() {
   words = loadStrings("magneticPoetryWordList.txt")
 }
@@ -7,30 +7,72 @@ function preload() {
 function setup() {
   createCanvas(400, 400)
   words = shuffle(words)
-  noLoop()
   rectMode(CENTER)
+  let x = Word.PADDING
+  let dy = Word.PADDING + textSize()
+  let y = dy;  
+  for( const word of words ) {
+    const w = new Word(word)
+    if( x + w.tw > width ) {
+      y += dy
+      x = Word.PADDING
+    }
+    w.position = createVector(x,y)
+    containers.push( w )
+    x += w.tw + Word.PADDING + Word.PADDING + Word.PADDING
+  }  
 }
 
 function draw() {
   background('white')
-  const padding = 2
-  let x = padding
-  let dy = padding + textSize()
-  let y = dy
 
-  for( const word of words ) {
-    const tw  = textWidth(word)
-    const blockWidth = tw + padding + padding
-    if( x + tw > width ) {
-      y += dy
-      x = padding
+  for( const word of containers ) {
+    if( word.isMouseOver() ) {
+      word.setHoverBG()
+    } else {
+      word.setDefaultBG()
     }
-    push();
-    translate(x + blockWidth/2,y, textSize()/2)
-    rotate(random(-PI/100,PI/100))
-    rect( 0, 0, blockWidth, textSize() )
-    text(word,0 - tw/2, 0 + textSize()/2)
-    pop()
-    x += tw + padding + padding + padding
+    word.draw()  
   }
 }
+
+class Word {
+  constructor(text) {
+    this.text = text
+    this.wiggle = random(-PI/100,PI/100)
+    this.position = createVector(0,0)
+    this.bg = color('white')
+  }
+  
+  get tw() { return textWidth(this.text) }
+  get blockWidth() { return this.tw + Word.PADDING + Word.PADDING }
+  
+  draw() {
+    push()
+    translate(
+      this.position.x + this.blockWidth/2,
+      this.position.y + textSize()/2 
+    ) 
+    rotate( this.wiggle ) 
+    fill(this.bg)
+    rect( 0, 0, this.blockWidth, textSize() ) 
+    fill("black")
+    text(this.text,0 - this.tw/2, 0 + textSize()/2 - Word.PADDING)
+    pop()   
+  }
+  
+  isMouseOver() { 
+    const hw = this.blockWidth/2
+    const hh = textSize()/2
+
+    return mouseX > this.position.x - hw && 
+      mouseX < this.position.x + hw &&
+      mouseY > this.position.y - hh &&
+      mouseY < this.position.y + hh
+  }
+  
+  setHoverBG() { this.bg = color(255,0,0,16) }
+  setDefaultBG() { this.bg = color('white') }
+}
+
+Word.PADDING = 2
