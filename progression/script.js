1b84961067b430d3e63fae48e158be01455377d4
Mr. Oswald <62300716+gajoswald@users.noreply.github.com>
2023-01-25 15:40:38 -0500

    Load and layout words
    loadStrings, textSize, textWidth

--- progression/script.js
+++ progression/script.js
@@ -1,7 +1,24 @@
+let words
+
+function preload() {
+  words = loadStrings("magneticPoetryWordList.txt")
+}
+
 function setup() {
   createCanvas(400, 400)
+  noLoop()
 }
 
 function draw() {
-  background(220)
+  background('white')
+  let x = 0
+  let y = textSize()
+  for( const word of words ) {
+    if( x + textWidth(word) > width ) {
+      y += textSize()
+      x = 0;
+    }
+    text(word,x,y)
+    x += textWidth(word)
+  }
 }
